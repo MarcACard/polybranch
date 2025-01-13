428a29ba6b3518d6b4f9527dbec7eb3da4557a02
Marc A Card <marcacard@gmail.com>
2025-01-13 12:28:20 -0500

    cleanup (canvas): remove unused imports

--- components/canvas/canvas.tsx
+++ components/canvas/canvas.tsx
@@ -1,25 +1,16 @@
 "use client";
 
-import { useCallback } from "react";
 import {
   ReactFlow,
   Background,
   Controls,
-  useNodesState,
-  useEdgesState,
-  type Node,
   type Edge,
   type NodeChange,
   type EdgeChange,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
-import { storage } from "@/lib/local-storage";
-import { StorageKeys } from "@/constants/storage-keys";
 import { MessageNode } from "@/components/canvas/message-node";
-import { Button } from "@/components/ui/button";
-import { Message } from "@/types/messages";
-import { logger } from "@/lib/logger";
 import { MessageNode as MessageNodeType } from "@/types/nodes";
 
 const nodeTypes = { message: MessageNode };
@@ -37,10 +28,6 @@ export function Canvas({
 }) {
   return (
     <>
-      <div className="absolute top-20 left-4 z-10">
-        {/* TODO: Re-implement test node functionality. */}
-        {/* <Button onClick={addTestNode}>Add Test Node</Button> */}
-      </div>
       <ReactFlow
         nodeTypes={nodeTypes}
         nodes={nodes}
